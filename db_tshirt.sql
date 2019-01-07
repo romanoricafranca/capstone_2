e892c139106bc5757815c0f05b3d52302faef6b7
r.ricafranca <ricafrancaromano@gmail.com>
2019-01-07 15:23:13 +0800

    capstone2-ecommerce

--- db_tshirt.sql
+++ db_tshirt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 22, 2018 at 02:59 PM
+-- Generation Time: Jan 07, 2019 at 08:13 AM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
@@ -40,8 +40,7 @@ CREATE TABLE `tbl_categories` (
 INSERT INTO `tbl_categories` (`id`, `name`) VALUES
 (1, 'Astronaut'),
 (2, 'Animals'),
-(3, 'Aliens'),
-(4, 'Soon to have more designs ...');
+(3, 'Aliens');
 
 -- --------------------------------------------------------
 
@@ -85,10 +84,25 @@ CREATE TABLE `tbl_orders` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `transaction_code` varchar(255) NOT NULL,
-  `purchase_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `purchase_date` varchar(255) NOT NULL,
   `status_id` int(11) DEFAULT NULL,
   `payment_mode_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_orders`
+--
+
+INSERT INTO `tbl_orders` (`id`, `user_id`, `transaction_code`, `purchase_date`, `status_id`, `payment_mode_id`) VALUES
+(1, 13, '8B1dD4B7a57A7D177ABE11-1546750693', '0000-00-00 00:00:00', 1, 1),
+(2, 13, '3Be2bd77aE95DC085B68ca-1546750863', '0000-00-00 00:00:00', 1, 1),
+(3, 13, 'E7B0451Ddd1B53F9F7BC25-1546750971', '0000-00-00 00:00:00', 1, 1),
+(4, 13, '5fa4EefbDEacbBF42f8dee-1546751411', '0000-00-00 00:00:00', 1, 1),
+(5, 13, '88ca9F5bdc8F1ff0EEE871-1546752790', 'January 6, 2019, Sun 01:33:10 pm', 3, 1),
+(6, 13, '04CAE96E719d6FC2c00f1b-1546835858', 'January 7, 2019, Mon 12:37:38 pm', 1, 1),
+(7, 13, '97Bd70F3f9470EDb88C554-1546835989', 'January 7, 2019, Mon 12:39:49 pm', 1, 1),
+(8, 15, 'e105Fbf15bA7d0Cad0f6E8-1546838374', 'January 7, 2019, Mon 01:19:34 pm', 1, 1),
+(9, 16, '4CDFBd7c3DC69FbCfaEf5a-1546838463', 'January 7, 2019, Mon 01:21:03 pm', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -100,9 +114,20 @@ CREATE TABLE `tbl_order_items` (
   `id` int(11) NOT NULL,
   `order_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL,
+  `sizes_id` int(11) DEFAULT NULL,
   `quantity` int(11) NOT NULL,
   `price` decimal(18,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_order_items`
+--
+
+INSERT INTO `tbl_order_items` (`id`, `order_id`, `item_id`, `sizes_id`, `quantity`, `price`) VALUES
+(1, 3, 10, 1, 1, '456.00'),
+(2, 3, 9, 1, 1, '299.00'),
+(3, 4, 9, 1, 4, '1196.00'),
+(4, 4, 11, 1, 2, '1796.00');
 
 -- --------------------------------------------------------
 
@@ -185,16 +210,10 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id`, `fname`, `lname`, `email`, `address`, `pw`) VALUES
-(1, '', '', 'gdf@rmail.dom', 'fdgd', '38fc6751c055a7edc420679c1fef24c18ef5606a'),
-(2, '', '', 'mail@rmail.com', 'address', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3'),
-(3, 'trytry', '', 'tytr@rmail.com', 'trytr', '0fbc9f8a865c5ddc2e8b74b4b01e931caf31f764'),
-(4, 'gfd', '', 'gdf@rmail.dom', 'gdf', '30e0c510958c33b6a29f8b7ec2b640fe022f80ad'),
-(5, 'gdfgf', '', 'fgdfgdf@email.dfd', 'f', 'da39a3ee5e6b4b0d3255bfef95601890afd80709'),
-(6, 'fgdf', '', 'dfgdf@fsdf.ghf', 'fggf', '72f77e84ba0149b2af1051f1318128dccf60ab60'),
-(7, 'fr', '', 'fdfsf@df.dg', 'er', 'fgd'),
-(8, 'Romano', 'Ricafranca', 'ricafrancaromano@gmail.com', 'antipolo city', 'da39a3ee5e6b4b0d3255bfef95601890afd80709'),
-(9, 'Admin', 'Admin', 'admin@gmail.com', 'antipolo city', 'da39a3ee5e6b4b0d3255bfef95601890afd80709'),
-(10, 'Test', 'Test', 'test@gmail.com', 'test', 'da39a3ee5e6b4b0d3255bfef95601890afd80709');
+(13, 'Test123', 'Test123', 'test123@yahoo.com', 'Mambugan Antipolo City', '448ed7416fce2cb66c285d182b1ba3df1e90016d'),
+(14, 'romano', 'Ricafranca', 'ricafrancaromano@gmail.com', 'Mambugan Antipolo City', '448ed7416fce2cb66c285d182b1ba3df1e90016d'),
+(15, 'Kieffer', 'Navarro', 'kieffer@gmail.com', 'quezon city', '448ed7416fce2cb66c285d182b1ba3df1e90016d'),
+(16, 'Loydie', 'Paraiso', 'loydie@gmail.com', '', '448ed7416fce2cb66c285d182b1ba3df1e90016d');
 
 --
 -- Indexes for dumped tables
@@ -268,25 +287,25 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_categories`
 --
 ALTER TABLE `tbl_categories`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_items`
 --
 ALTER TABLE `tbl_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbl_orders`
 --
 ALTER TABLE `tbl_orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_order_items`
 --
 ALTER TABLE `tbl_order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_payment_mode`
@@ -310,7 +329,7 @@ ALTER TABLE `tbl_status`
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
@@ -329,13 +348,6 @@ ALTER TABLE `tbl_orders`
   ADD CONSTRAINT `tbl_orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tbl_users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `tbl_orders_ibfk_2` FOREIGN KEY (`status_id`) REFERENCES `tbl_status` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `tbl_orders_ibfk_3` FOREIGN KEY (`payment_mode_id`) REFERENCES `tbl_payment_mode` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `tbl_order_items`
---
-ALTER TABLE `tbl_order_items`
-  ADD CONSTRAINT `tbl_order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tbl_orders` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbl_order_items_ibfk_2` FOREIGN KEY (`item_id`) REFERENCES `tbl_items` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
